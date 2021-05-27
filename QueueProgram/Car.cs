97a252792bbbaa73480613d275d461954189571d
K NIKHIL KUMAR REDDY <nikhilkumarreddy.kv@gmail.com>
2021-05-27 20:30:51 +0530

    string s in 301

--- QueueProgram/Car.cs
+++ QueueProgram/Car.cs
@@ -6,6 +6,7 @@ namespace QueueProgram
 {
     class Car
     {
+        string s = "we are in f301";
         private void colourCar()
         {
             Console.WriteLine("this is car red  colur");
