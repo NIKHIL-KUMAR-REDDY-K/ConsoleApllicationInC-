56d011628da745177507694a12913b3527eb2df3
K NIKHIL KUMAR REDDY <nikhilkumarreddy.kv@gmail.com>
2021-06-01 09:52:36 +0530

    add1line

--- QueueProgram/Car.cs
+++ QueueProgram/Car.cs
@@ -6,7 +6,7 @@ namespace QueueProgram
 {
     class Car
     {
-        string s = "we are in f1000";
+        string sa = "we are in f1000inb2";
         private void wheels()
         {
             Console.WriteLine("it has four wheels");
