b714628082bfcf62b11b586c045f94017c9967a9
K NIKHIL KUMAR REDDY <nikhilkumarreddy.kv@gmail.com>
2021-06-01 09:55:41 +0530

    fc

--- QueueProgram/Car.cs
+++ QueueProgram/Car.cs
@@ -6,12 +6,9 @@ namespace QueueProgram
 {
     class Car
     {
-<<<<<<< HEAD
         string s = "we are in f3000";
-=======
-        string s = "we are in f200";
-        string s = "we are in f100";
->>>>>>> c8155dec488de015314a0a0fdeb09d257943409c
+        string s1 = "we are in f200";
+        string s2 = "we are in f100";
         private void wheels()
         {
             Console.WriteLine("it has four wheels");
